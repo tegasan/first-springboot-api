5d2f3b5df5e8596d35191b49348f91591195044a
paul tegasan <tegasan@gmail.com>
2021-01-02 07:45:57 -0600

    first initial project

---
+++ src/main/java/com/javateg/springboot/startone/startoneapi/StartOneApiApplication.java
@@ -0,0 +1,13 @@
+package com.javateg.springboot.startone.startoneapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartOneApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartOneApiApplication.class, args);
+	}
+
+}
